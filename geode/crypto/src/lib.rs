413a4b8c28d133a78e43ceaa9b78722b49cc35e1
Chua Zheng Leong <czl@iiyume.org>
2021-02-14 02:13:40 +0800

    Fix test. Clean up unused vars + imports

--- geode/crypto/src/lib.rs
+++ geode/crypto/src/lib.rs
@@ -29,7 +29,6 @@ pub use enclave::*;
 mod test {
     use super::*;
     use geode_types::*;
-    use serde_json::*;
 
     #[test]
     fn test_sign() {
@@ -37,13 +36,10 @@ mod test {
             hex::decode("60ed0dd24087f00faea4e2b556c74ebfa2f0e705f8169733b01530ce4c619883")
                 .unwrap();
         let prvkey: Secp256k1PrivateKey = prvkey_bytes.as_slice().into();
-        let addr = secp256k1_public_from_private(&prvkey).unwrap();
-        let acctid = eth_secp256k1_to_accountid(&addr);
 
-        let voter = (0x9999343212322323 as u64).into();
+        let voter = parse_string_h256("0x9999343212322323");
         let proposal = (0x1234567892232 as u64).into();
         let option = 1;
-        //let timestamp:u64 = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap().as_secs();
         let timestamp: u64 = 1612273931;
 
         let vote = Vote::new(voter, proposal, option, timestamp);
@@ -63,7 +59,7 @@ mod test {
 
     #[test]
     fn test_verify() {
-        let signed_vote_json = r#"{"msg":{"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Vote":[{"name":"voter","type":"uint256"},{"name":"proposal","type":"uint256"},{"name":"option","type":"uint32"},{"name":"timestamp","type":"uint64"}]},"domain":{"name":"Witness","version":"0.1.0","chainId":"86","verifyingContract":"0x0101010101010101010101010101010101010101"},"primaryType":"Vote","message":{"voter":"11067934948897989411","proposal":"320255973466674","option":1,"timestamp":1612273931}},"v":27,"r":"0xd925d9a6ffd1b533604db6bf4f6db37572cf5d8e4b47d95c12f92bbf6779b8ce","s":"0x4991b434e723375320b40693e50bc3b03caf0c6463fb50fbfcee394d458c2a75"}"#;
+        let signed_vote_json = r#"{"msg":{"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Vote":[{"name":"voter","type":"uint256"},{"name":"proposal","type":"uint256"},{"name":"option","type":"uint32"},{"name":"timestamp","type":"uint64"}]},"domain":{"name":"Witness","version":"0.1.0","chainId":"86","verifyingContract":"0x0101010101010101010101010101010101010101"},"primaryType":"Vote","message":{"voter":"0x0000000000000000000000000000000000000000000000009999343212322323","proposal":"320255973466674","option":1,"timestamp":1612273931}},"v":27,"r":"0xd925d9a6ffd1b533604db6bf4f6db37572cf5d8e4b47d95c12f92bbf6779b8ce","s":"0x4991b434e723375320b40693e50bc3b03caf0c6463fb50fbfcee394d458c2a75"}"#;
         let signed_vote: EIP712SignedMsg<Vote> = serde_json::from_str(signed_vote_json).unwrap();
         let pubkey = secp256k1_eip712_verify(&signed_vote).unwrap();
         let account_id = eth_secp256k1_to_accountid(&pubkey);
