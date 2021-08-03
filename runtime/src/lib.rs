faa99ab9414966ac9fa18dd1e420f3ed6d786b39
RyuH1 <haichuan@ata.network>
2021-08-03 17:11:39 +0800

    renaming pallet-attestation to pallet-liveness

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -63,7 +63,7 @@ use pallet_evm::{
 pub use pallet_template;
 pub use pallet_attestor;
 pub use pallet_geode;
-pub use pallet_attestation;
+pub use pallet_liveness;
 
 pub use automata_primitives::*;
 
@@ -390,7 +390,7 @@ impl pallet_geode::Config for Runtime {
 	type Event = Event;
 }
 
-impl pallet_attestation::Config for Runtime {
+impl pallet_liveness::Config for Runtime {
 	type Event = Event;
 }
 
@@ -435,7 +435,7 @@ construct_runtime!(
 		Indices: pallet_indices::{Module, Call, Storage, Config<T>, Event<T>},
 		AttestorModule: pallet_attestor::{Module, Call, Storage, Event<T>},
 		GeodeModule: pallet_geode::{Module, Call, Storage, Event<T>},
-		AttestationModule: pallet_attestation::{Module, Call, Storage, Event<T>},
+		LivenessModule: pallet_liveness::{Module, Call, Storage, Event<T>},
 	}
 );
 
