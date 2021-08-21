f9885c6cf06a3d54b52314ce286ffc0fb415f1c5
Liao1 <liaoyi1994@gmail.com>
2021-08-21 17:48:34 +0800

    fix mock

--- pallets/transfer/src/mock.rs
+++ pallets/transfer/src/mock.rs
@@ -71,6 +71,14 @@ impl pallet_balances::Config for Test {
     type WeightInfo = ();
 }
 
+impl<C> frame_system::offchain::SendTransactionTypes<C> for Test
+where
+    Call: From<C>,
+{
+    type Extrinsic = UncheckedExtrinsic;
+    type OverarchingCall = Call;
+}
+
 impl transfer::Config for Test {
     type Event = Event;
     type Currency = Balances;
