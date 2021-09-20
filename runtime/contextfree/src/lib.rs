52a85e9b9c66e37a64af6d3cd8107675bd3feea6
Liao1 <liaoyi1994@gmail.com>
2021-09-20 14:42:39 +0800

    update contextfree reward curve

--- runtime/contextfree/src/lib.rs
+++ runtime/contextfree/src/lib.rs
@@ -369,8 +369,8 @@ impl pallet_election_provider_multi_phase::Config for Runtime {
 // pallet_staking_reward_curve::build! {
 //     // 4.5% min, 27.5% max, 50% ideal stake
 //     const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-//         min_inflation: 0_045_000,
-//         max_inflation: 0_275_000,
+//         min_inflation: 0_025_000,
+//         max_inflation: 0_100_000,
 //         ideal_stake: 0_500_000,
 //         falloff: 0_050_000,
 //         max_piece_count: 40,
