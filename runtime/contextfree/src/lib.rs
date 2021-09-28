7448557d34ebd30994a5b4361a2de8ea7e83be4d
RyuH1 <haichuan@ata.network>
2021-09-28 15:48:57 +0800

    Update Spec to 124 and Enable Staking NPoS Governance

--- runtime/contextfree/src/lib.rs
+++ runtime/contextfree/src/lib.rs
@@ -125,7 +125,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
     // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
     //   the compatible custom types.
-    spec_version: 119,
+    spec_version: 124,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
@@ -172,10 +172,13 @@ impl Contains<Call> for CallFilter {
             | Call::Indices(_)
             | Call::Babe(_)
             | Call::Sudo(_)
-            | Call::Timestamp(_) => true,
-
-            // These modules are not allowed to be called by transactions:
-            Call::EVM(_)
+            | Call::Staking(_)
+            | Call::Session(_)
+            | Call::Utility(_)
+            | Call::Balances(_)
+            | Call::ElectionProviderMultiPhase(_)
+            | Call::BridgeTransfer(_)
+            | Call::ChainBridge(_)
             | Call::Democracy(_)
             | Call::Council(_)
             | Call::TechnicalCommittee(_)
@@ -183,14 +186,11 @@ impl Contains<Call> for CallFilter {
             | Call::Treasury(_)
             | Call::PhragmenElection(_)
             | Call::Scheduler(_)
-            | Call::Balances(_)
-            | Call::BridgeTransfer(_)
-            | Call::ChainBridge(_)
+            | Call::Timestamp(_) => true,
+
+            // These modules are not allowed to be called by transactions:
+            Call::EVM(_)
             | Call::Vesting(_)
-            | Call::Staking(_)
-            | Call::ElectionProviderMultiPhase(_)
-            | Call::Session(_)
-            | Call::Utility(_)
             | Call::Ethereum(_) => false,
         }
     }
@@ -372,26 +372,26 @@ impl pallet_election_provider_multi_phase::Config for Runtime {
     type WeightInfo = pallet_election_provider_multi_phase::weights::SubstrateWeight<Runtime>;
 }
 
-// pallet_staking_reward_curve::build! {
-//     // 4.5% min, 27.5% max, 50% ideal stake
-//     const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-//         min_inflation: 0_025_000,
-//         max_inflation: 0_100_000,
-//         ideal_stake: 0_500_000,
-//         falloff: 0_050_000,
-//         max_piece_count: 40,
-//         test_precision: 0_005_500,
-//     );
-// }
+pallet_staking_reward_curve::build! {
+    // 4.5% min, 27.5% max, 50% ideal stake
+    const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
+        min_inflation: 0_025_000,
+        max_inflation: 0_100_000,
+        ideal_stake: 0_500_000,
+        falloff: 0_050_000,
+        max_piece_count: 40,
+        test_precision: 0_005_500,
+    );
+}
 
-const REWARD_CURVE: PiecewiseLinear<'static> = PiecewiseLinear {
-    points: &[
-        (Perbill::from_parts(0), Perbill::from_parts(0)),
-        (Perbill::from_parts(0_500_000_000), Perbill::from_parts(0)),
-        (Perbill::from_parts(1_000_000_000), Perbill::from_parts(0)),
-    ],
-    maximum: Perbill::from_parts(0),
-};
+// const REWARD_CURVE: PiecewiseLinear<'static> = PiecewiseLinear {
+//     points: &[
+//         (Perbill::from_parts(0), Perbill::from_parts(0)),
+//         (Perbill::from_parts(0_500_000_000), Perbill::from_parts(0)),
+//         (Perbill::from_parts(1_000_000_000), Perbill::from_parts(0)),
+//     ],
+//     maximum: Perbill::from_parts(0),
+// };
 
 parameter_types! {
     pub const SessionsPerEra: sp_staking::SessionIndex = 6;
