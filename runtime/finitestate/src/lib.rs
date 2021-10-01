68dffbb49a3511935af2048866acb2a8a8043f85
RyuH1 <haichuan@ata.network>
2021-10-01 15:19:29 +0800

    Update frontier ChainId

--- runtime/finitestate/src/lib.rs
+++ runtime/finitestate/src/lib.rs
@@ -841,7 +841,7 @@ impl<F: FindAuthor<u32>> FindAuthor<H160> for FindAuthorTruncated<F> {
 
 // EVM parameters
 parameter_types! {
-    pub const ChainId: u64 = 86;
+    pub const ChainId: u64 = 13107;
     pub BlockGasLimit: U256 = U256::from(u32::max_value());
 }
 
