6c75425925da58819dea56b6f752a8409c91a707
RyuH1 <79819095+RyuH1@users.noreply.github.com>
2021-10-28 07:43:42 +0000

    Fix build script
    set finitestate as default feature
    
    Run local two nodes testnet
    
    Fix build script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This script meant to be run on Unix/Linux based systems
+
 set -e
 
 echo "*** Initializing WASM build environment"
@@ -10,3 +10,7 @@ if [ -z $CI_PROJECT_NAME ] ; then
 fi
 
 rustup target add wasm32-unknown-unknown --toolchain nightly
+
+# Install wasm-gc. It's useful for stripping slimming down wasm binaries.
+command -v wasm-gc || \
+	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
